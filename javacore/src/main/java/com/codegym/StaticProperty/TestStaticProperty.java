9211b3a315d000254e46b6fe8536d5e504db12b3
Nguyen <nguyennp.htra.031004@gmail.com>
2022-10-20 09:59:45 +0700

    helo

---
+++ javacore/src/main/java/com/codegym/StaticProperty/TestStaticProperty.java
@@ -0,0 +1,16 @@
+package com.codegym.StaticProperty;
+
+public class TestStaticProperty {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car("Mazda 3","Skyactiv 3");
+
+        System.out.println(Car.numberOfCars);
+
+        Car car2 = new Car("Mazda 6","Skyactiv 6");
+
+        System.out.println(Car.numberOfCars);
+
+    }
+}
