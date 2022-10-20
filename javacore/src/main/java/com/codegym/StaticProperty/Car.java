1f95b9964017771c9b16e41b9633789bd1d4711b
Sen20312123 <sen20312123@gmail.com>
2022-10-20 10:00:16 +0700

    nhan-thuchanh

---
+++ javacore/src/main/java/com/codegym/StaticProperty/Car.java
@@ -0,0 +1,23 @@
+package com.codegym.StaticProperty;
+
+public class Car {
+    private String name;
+
+    private String engine;
+
+
+
+    public static int numberOfCars;
+
+
+
+    public Car(String name, String engine) {
+
+        this.name = name;
+
+        this.engine = engine;
+
+        numberOfCars++;
+
+    }
+}
