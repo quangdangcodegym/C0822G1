11aecc5f74949c0398710c3fb4c37729eaa6955e
Sen20312123 <sen20312123@gmail.com>
2022-10-26 08:35:21 +0700

    java

--- javacore/src/main/java/com/codegym/extendsoop/compare/demo/CircleComparatorTest.java
+++ javacore/src/main/java/com/codegym/extendsoop/compare/demo/CircleComparatorTest.java
@@ -7,7 +7,7 @@ public class CircleComparatorTest {
     public static void main(String[] args) {
         Circle[] circles = new Circle[3];
         circles[0] = new Circle(3.6);
-        circles[1] = new Circle();
+        circles[1] = new Circle(1);
         circles[2] = new Circle(3.5, "indigo", false);
 
         System.out.println("Pre-sorted:");
