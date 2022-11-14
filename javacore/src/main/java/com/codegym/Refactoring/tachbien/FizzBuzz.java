94356da3849b21b82552ce310a8e3d1a4bb02919
Sen20312123 <sen20312123@gmail.com>
2022-11-14 08:26:49 +0700

    demo

--- javacore/src/main/java/com/codegym/Refactoring/tachbien/FizzBuzz.java
+++ javacore/src/main/java/com/codegym/Refactoring/tachbien/FizzBuzz.java
@@ -1,24 +1,38 @@
 package com.codegym.Refactoring.tachbien;
 
-
 public class FizzBuzz {
     public static String fizzBuzz(int number){
-        boolean isFizz = number % 3 == 0;
-        boolean isBuzz = number % 5 == 0;
-
-        if(isFizz && isBuzz)
+        if(number % 3 == 0 && number % 5 == 0)
             return "FizzBuzz";
 
-        if(isFizz)
+        if(number % 3 == 0)
             return "Fizz";
 
-        if(isBuzz)
+        if(number % 5 == 0)
             return "Buzz";
 
         return number + "";
     }
-
 }
+
+//public class FizzBuzz {
+//    public static String fizzBuzz(int number){
+//        boolean isFizz = number % 3 == 0;
+//        boolean isBuzz = number % 5 == 0;
+//
+//        if(isFizz && isBuzz)
+//            return "FizzBuzz";
+//
+//        if(isFizz)
+//            return "Fizz";
+//
+//        if(isBuzz)
+//            return "Buzz";
+//
+//        return number + "";
+//    }
+//
+//}
 
 
 
--- javacore/src/main/java/com/codegym/Refactoring/tachphuongthuc/Cylinder.java
+++ javacore/src/main/java/com/codegym/Refactoring/tachphuongthuc/Cylinder.java
@@ -2,17 +2,26 @@ package com.codegym.Refactoring.tachphuongthuc;
 
 public class Cylinder {
     public static double getVolume(int radius, int height){
-        double baseArea = getBaseArea(radius);
-        double perimeter = getPerimeter(radius);
+        double baseArea = Math.PI * radius * radius;
+        double perimeter = 2 * Math.PI  * radius;
         double volume = perimeter * height + 2 * baseArea;
         return volume;
     }
-
-    private static double getPerimeter(int radius) {
-        return 2 * Math.PI  * radius;
-    }
-
-    private static double getBaseArea(int radius) {
-        return Math.PI * radius * radius;
-    }
 }
+
+//public class Cylinder {
+//    public static double getVolume(int radius, int height){
+//        double baseArea = getBaseArea(radius);
+//        double perimeter = getPerimeter(radius);
+//        double volume = perimeter * height + 2 * baseArea;
+//        return volume;
+//    }
+//
+//    private static double getPerimeter(int radius) {
+//        return 2 * Math.PI  * radius;
+//    }
+//
+//    private static double getBaseArea(int radius) {
+//        return Math.PI * radius * radius;
+//    }
+//}
